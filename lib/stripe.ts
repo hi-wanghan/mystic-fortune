54490397847430d16e0931a8d2a3799c6e2bf2e0
hi-wanghan <sport.hanwang@gmail.com>
2025-11-14 21:31:33 +0800

    upgrade stripe to latest
    based on doubao

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 ﻿import Stripe from 'stripe';
 import { loadStripe } from '@stripe/stripe-js';
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-06-20' });
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2025-09-30.clover' });
 let stripePromise: any;
 export const getStripe = () => {
   if (!stripePromise) stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
